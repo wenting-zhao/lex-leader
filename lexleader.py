6965386880053be35afafc50d39a4134745576f3
zhao <wentingzhao@outlook.com>
2017-08-20 13:31:17 -0500

    Lots of coding refactoring; added OR CSE encoding.

--- lexleader.py
+++ lexleader.py
@@ -2,9 +2,10 @@ import sys
 
 
 class LexLeader:
-    def __init__(self, columns, rows):
+    def __init__(self, columns, rows, option="and"):
         self.num_columns = columns
         self.num_rows = rows
+        self.parse_option(option)
         self.varmap = dict()
         self.num_var = 0
         for c in range(columns):
@@ -12,53 +13,58 @@ class LexLeader:
                 self.num_var += 1
                 self.varmap[(c, r)] = self.num_var
 
-    def big_and(self):
-        # dealing with columns...
-        for c in range(self.num_columns-1):
-            extra = ''
-            if c != 0:
-                extra = '& '
-            print (extra+"(x{} | !x{})".format(self.varmap[c, 0], self.varmap[c+1, 0]))
-            for r in range(1, self.num_rows):
-                output = '& ('
-                for x in range(r):
-                    if x != 0:
-                        output += ' & '
-                    output += "(x{0} = x{1})".format(self.varmap[c, x], self.varmap[c+1, x])
-                output += ') -> '
-                output += ("(x{} | !x{})".format(self.varmap[c, r], self.varmap[c+1, r]))
-                print (output)
+    def parse_option(self, option):
+        if option == "and":
+            self.which_lex = self._and_helper
+        elif option == "and-cse":
+            self.which_lex = self._and_subexpr_helper
+        elif option == "or":
+            self.which_lex = self._or_helper
+        elif option == "or-cse":
+            self.which_lex = self._or_subexpr_helper
 
-        # dealing with rows...
-        for r in range(self.num_rows-1):
-            print ("& (x{} | !x{})".format(self.varmap[0, r], self.varmap[0, r+1]))
-            for c in range(1, self.num_columns):
-                output = '& ('
-                for x in range(c):
-                    if x != 0:
-                        output += ' & '
-                    output += "(x{0} = x{1})".format(self.varmap[x, r], self.varmap[x, r+1])
-                output += ') -> '
-                output += ("(x{} | !x{})".format(self.varmap[c, r], self.varmap[c, r+1]))
-                print (output)
-
-    def recursive_and(self):
+    def print_lexleader(self):
         """ prints out the row and column lex-leader constraints of the full matrix
-            using the AND decomposition encoding using common sub-expression elimination
         """
-        res = []
+        full = []
         for c in range(self.num_columns-1):
             column1 = [self.varmap[(c, r)] for r in range(self.num_rows)]
             column2 = [self.varmap[(c+1, r)] for r in range(self.num_rows)]
-            res.append(self._helper_recursive_and(column1, column2))
+            full.append(self.which_lex(column1, column2))
         for r in range(self.num_rows-1):
             row1 = [self.varmap[(c, r)] for c in range(self.num_columns)]
             row2 = [self.varmap[(c, r+1)] for c in range(self.num_columns)]
-            res.append(self._helper_recursive_and(row1, row2))
-        print ("\n& ".join(res))
+            full.append(self.which_lex(row1, row2))
+        print ("\n& ".join(full))
 
-    def _recursive_and_helper(self, vector1, vector2):
+    def _and_helper(self, vector1, vector2):
         """ creates the lex-leader constraints between two vectors of variables
+            via the plain AND decomposition encoding
+            inputs:
+                vector1, vector2: lists of integers, equivalent lengths,
+                                  each representing a vector of variables
+            returns:
+                string containing the full expression of the lex-leader constraint
+        """
+        # setup vectors with 1-based indexing to match constraints in the source paper
+        A = [None] + vector1
+        B = [None] + vector2
+
+        res = []
+        res.append( "(!x{} | x{})".format(A[1], B[1]) )
+
+        assert len(vector1) == len(vector2)
+        for i in range(1, len(vector1)):
+            temp = []
+            for j in range(1, i+1):
+                temp.append( "(x{} = x{})".format(A[j], B[j]) )
+            temp = " & ".join(temp)
+            res.append( "({} -> (!x{} | x{}))".format(temp, A[i+1], B[i+1]) )
+        return "("+"\n& ".join(res)+")"
+
+    def _and_subexpr_helper(self, vector1, vector2):
+        """ creates the lex-leader constraints between two vectors of variables
+            via the AND decomposition encoding using common sub-expression elimination
             inputs:
                 vector1, vector2: lists of integers, equivalent lengths,
                                   each representing a vector of variables
@@ -83,68 +89,92 @@ class LexLeader:
         # X[1] <=> (A[1] = B[1])   (thesis, 3.19)
         res.append( "(x{} = (x{} = x{}))".format(X[1], A[1], B[1]) )
 
-        # 1 <= i <= n-2 X[i+1] <=> (X[i] & (A[i+1] = B[i+1]))   (thesis, 3.20)
+        # 1 <= i <= n-2, X[i+1] <=> (X[i] & (A[i+1] = B[i+1]))   (thesis, 3.20)
         for i in range(1, len(vector1)-1):
             res.append( "(x{} = (x{} & (x{} = x{})))".format(X[i+1], X[i], A[i+1], B[i+1]) )
-        # i <= i <= n-1 X[i] -> (A[i+1] <= B[i+1])   (thesis, 3.21)
+        # i <= i <= n-1, X[i] -> (A[i+1] <= B[i+1])   (thesis, 3.21)
         for i in range(1, len(vector1)):
             res.append( "(x{} -> (!x{} | x{}))".format(X[i], A[i+1], B[i+1]) )
 
-        return "\n& ".join(res)
+        return "("+"\n& ".join(res)+")"
 
-    def big_or(self):
-        # dealing with columns...
-        for c in range(self.num_columns-1):
-            extra = '('
-            if c != 0:
-                extra = '& ' + extra
-            print (extra+"(x{} & !x{})".format(self.varmap[c, 0], self.varmap[c+1, 0]))
-            for r in range(self.num_rows-1):
-                output = '| ('
-                for x in range(r+1):
-                    if x != 0:
-                        output += ' & '
-                    output += "(x{0} = x{1})".format(self.varmap[c, x], self.varmap[c+1, x])
-                output += " & (x{} & !x{})".format(self.varmap[c, r+1], self.varmap[c+1, r+1])
-                output += ')'
-                print (output)
-            more = '| ('
-            for r in range(self.num_rows):
-                if r != 0:
-                    more += ' & '
-                more += "(x{0} = x{1})".format(self.varmap[c, r], self.varmap[c+1, r])
-                if r == self.num_rows-1:
-                    more += '))'
-            print (more)
+    def _or_helper(self, vector1, vector2):
+        """ creates the lex-leader constraints between two vectors of variables
+            via the plain OR decomposition encoding
+            inputs:
+                vector1, vector2: lists of integers, equivalent lengths,
+                                  each representing a vector of variables
+            returns:
+                string containing the full expression of the lex-leader constraint
+        """
+        # setup vectors with 1-based indexing to match constraints in the source paper
+        A = [None] + vector1
+        B = [None] + vector2
 
-        # dealing with rows
-        for r in range(self.num_rows-1):
-            extra = '& ('
-            print (extra+"(x{} & !x{})".format(self.varmap[0, r], self.varmap[0, r+1]))
-            for c in range(self.num_columns-1):
-                output = '| ('
-                for x in range(c+1):
-                    if x != 0:
-                        output += ' & '
-                    output += "(x{0} = x{1})".format(self.varmap[x, r], self.varmap[x, r+1])
-                output += " & (x{} & !x{})".format(self.varmap[c+1, r], self.varmap[c+1, r+1])
-                output += ')'
-                print (output)
-            more = '| ('
-            for c in range(self.num_columns):
-                if c != 0:
-                    more += ' & '
-                more += "(x{0} = x{1})".format(self.varmap[c, r], self.varmap[c, r+1])
-                if c == self.num_columns-1:
-                    more += '))'
-            print (more)
+        res = []
+        res.append( "(!x{} & x{})".format(A[1], B[1]) )
+
+        assert len(vector1) == len(vector2)
+        for i in range(1, len(vector1)):
+            temp = []
+            for j in range(1, i+1):
+                temp.append( "(x{} = x{})".format(A[j], B[j]) )
+            temp = " & ".join(temp)
+            res.append( "({} -> (!x{} & x{}))".format(temp, A[i+1], B[i+1]) )
+
+        temp = []
+        for i in range(1, len(vector1)+1):
+            temp.append( "(x{} = x{})".format(A[i], B[i]) )
+        res.append(" & ".join(temp))
+
+        return "("+"\n| ".join(res)+")"
+
+    def _or_subexpr_helper(self, vector1, vector2):
+        """ creates the lex-leader constraints between two vectors of variables
+            via the OR decomposition encoding using common sub-expression elimination
+            inputs:
+                vector1, vector2: lists of integers, equivalent lengths,
+                                  each representing a vector of variables
+            returns:
+                string containing the full expression of the lex-leader constraint
+        """
+        A = [None] + vector1
+        B = [None] + vector2
+
+        # creating the extra variables
+        X = dict()
+        assert len(vector1) == len(vector2)
+        for i in range(1, len(vector1)):
+            self.num_var += 1
+            X[i] = self.num_var
+
+        res = []  # for ANDing each element...
+        temp = []  # for ORing each element...
+
+        # A[1] < B[1]
+        temp.append( "(!x{} & x{})".format(A[1], B[1]) )
+        # 1 <= i <= n-1, X[i] & (A[i+1] < B[i+1]))
+        for i in range(1, len(vector1)):
+            temp.append( "(x{} & (!x{} & x{}))".format(X[1], A[i+1], B[i+1]) )
+        # A[n] = B[n]
+        temp.append( "x{}".format(X[len(vector1)-1]) )
+        res.append( "("+" | ".join(temp)+")" )
+
+        # X[1] <=> A[1] = B[1]   (thesis, 3.36)
+        res.append( "(x{} = (x{} = x{}))".format(X[1], A[1], B[1]) )
+        # 1 <= i <= n−1, X[i+1] <=> (X[i] & (A[i+1] = B[i+1]))   (thesis, 3.37)
+        for i in range(1, len(vector1)-1):
+            res.append( "(x{} = (x{} & (x{} = x{})))".format(X[i+1], X[i], A[i+1], B[i+1]) )
+
+        return "("+"\n& ".join(res)+")"
 
 
 def main():
     columns = int(sys.argv[1])
     rows = int(sys.argv[2])
-    lex = LexLeader(columns, rows)
-    lex.recursive_and()
+    option = sys.argv[3]
+    lex = LexLeader(columns, rows, option)
+    lex.print_lexleader()
 
 if __name__ == '__main__':
     main()
