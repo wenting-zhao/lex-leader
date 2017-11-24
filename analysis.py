326c3a4c0f3953ead0ec2024bfa43e3e06df99c4
zhao <wentingzhao@outlook.com>
2017-11-24 14:15:20 -0600

    Added the code to analysis program output.

---
+++ analysis.py
@@ -0,0 +1,30 @@
+import os
+
+everything = ["and", "and-cse", "or", "or-cse", "ror", "alpha", "alpha-m", "harvey", "mylex", "none"]
+
+
+def main():
+    directory = os.fsencode("oneresult")
+    allresults = dict()
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".results"):
+            pathtofile = os.path.join(directory.decode(), filename)
+            with open(pathtofile) as f:
+                results = dict()
+                for line in f:
+                    split = line.split(',')
+                    results[split[0]] = split[-1][:-2]
+            allresults[filename.split('.')[0]] = results
+    print(','+','.join(everything))
+    for instance in allresults.keys():
+        result2print = []
+        for which in everything:
+            result = allresults[instance]
+            if which in result:
+                result2print.append(result[which])
+            else:
+                result2print.append(str(99999999))
+        print(instance.replace(',', '-')+','+','.join(result2print))
+main()
